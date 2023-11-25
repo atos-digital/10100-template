d090c1117c8b8083bc5e71728b763a4acb377224
Steven Lee <steven.lee@kynrai.com>
2023-11-25 00:41:03 +0000

    chore: make header/footer more visible

--- ui/header_templ.go
+++ ui/header_templ.go
@@ -23,7 +23,7 @@ func Header() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<header><div class=\"flex justify-center\"><h1 class=\"bg-rose-500\">")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<header><div class=\"flex justify-center bg-blue-400\"><h1>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
